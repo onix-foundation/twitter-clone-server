7a6870f1dd421510f4b4805251da664aec2189da
SGDIEGO <diegoalonsosg@gmail.com>
2024-11-03 18:56:49 -0500

    feat(TC-25): update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { setupSecurity } from './common/infrastructure/middleware/security';
 import { errorHandler } from './shared/errors/errorHandler';
-import userRouter from "./contexts/user/interfaces/routes"
+import userRouter from './contexts/user/interfaces/routes';
 
 const app = express();
 setupSecurity(app);
 app.use(express.json());
 
 // app.use('/api/v1');
-app.use(userRouter)
+app.use(userRouter);
 
 app.use(errorHandler);
 export { app };
